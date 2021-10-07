a474b6fab7d8d6dc7105b0681763f639afb37146
Daniel Watson <d.m.watson1985@gmail.com>
2021-10-07 17:22:48 +0100

    Initial Commit

---
+++ Filter/FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterApp.swift
+//  Filter
+//
+//  Created by Daniel Watson on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
