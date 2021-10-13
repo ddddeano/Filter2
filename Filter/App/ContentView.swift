521d1623e859a33e795bb87ff3382c5b8fe8e0eb
Deano <d.m.watson1985@gmail.com>
2021-10-13 12:55:40 +0100

    Now takes user location more literally,  showing street view.

--- Filter/App/ContentView.swift
+++ Filter/App/ContentView.swift
@@ -1,12 +1,13 @@
 //
 //  ContentView.swift
-//  Filter
+//  MapView
 //
 //  Created by Daniel Watson on 13/10/2021.
 //
 
 import SwiftUI
 import MapKit
+
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
@@ -31,14 +32,41 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     
 }
 
+
+class Coordinator: NSObject, MKMapViewDelegate {
+    
+    var mapView: MapView
+    
+    init(_ mapView: MapView) {
+        self.mapView = mapView
+    }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        if let annotationView = views.first {
+            if let annotation = annotationView.annotation {
+                if annotation is MKUserLocation {
+                    
+                    let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                    mapView.setRegion(region, animated: true)
+                    
+                }
+            }
+        }
+    }
+}
+
+
 struct MapView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> MKMapView {
-        return MKMapView()
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.delegate = context.coordinator
+        return map
     }
     
     func makeCoordinator() -> Coordinator {
-//
+        Coordinator(self)
     }
     
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
@@ -46,9 +74,13 @@ struct MapView: UIViewRepresentable {
     }
 }
 
+
+
+
 struct ContentView: View {
     
     @ObservedObject var locationManager = LocationManager()
+    
     var body: some View {
         MapView()
             .edgesIgnoringSafeArea(.all)
