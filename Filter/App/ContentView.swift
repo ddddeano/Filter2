5e8619360960d18d3a12fdf64b6a54b50a25e171
Deano <d.m.watson1985@gmail.com>
2021-10-15 10:49:20 +0100

    Finished main part of the project

--- Filter/App/ContentView.swift
+++ Filter/App/ContentView.swift
@@ -9,6 +9,33 @@ import SwiftUI
 import MapKit
 
 
+final class LandmarkAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(landmark: Landmark) {
+        self.title = landmark.name
+        self.coordinate = landmark.coordinate
+    }
+}
+
+struct Landmark {
+    let placemark: MKPlacemark
+    
+    var id:UUID {
+        return UUID()
+    }
+    var name: String {
+        self.placemark.name ?? ""
+    }
+    var title: String {
+        self.placemark.title ?? ""
+    }
+    var coordinate: CLLocationCoordinate2D {
+        self.placemark.coordinate
+    }
+}
+
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var location: CLLocation? = nil
@@ -58,6 +85,9 @@ class Coordinator: NSObject, MKMapViewDelegate {
 
 struct MapView: UIViewRepresentable {
     
+    
+    var landmarks: [Landmark ]
+    
     func makeUIView(context: Context) -> MKMapView {
         let map = MKMapView()
         map.showsUserLocation = true
@@ -70,7 +100,13 @@ struct MapView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-//
+         updateAnnotations(from: uiView)
+    }
+    
+    private func updateAnnotations(from mapView: MKMapView){
+        mapView.removeAnnotations(mapView.annotations)
+        let annotations = self.landmarks.map(LandmarkAnnotation.init)
+        mapView.addAnnotations(annotations)
     }
 }
 
@@ -80,15 +116,83 @@ struct MapView: UIViewRepresentable {
 struct ContentView: View {
     
     @ObservedObject var locationManager = LocationManager()
+    @State private var landmarks: [Landmark] = [Landmark]()
+    @State private var search = ""
+    @State var isTapped = false
+    
+    private func getNearbyLandmarks() {
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        
+        let search = MKLocalSearch(request: request)
+        search.start { response, error in
+            if let response = response {
+                
+                let mapItems = response.mapItems
+                self.landmarks = mapItems.map {
+                    Landmark(placemark: $0.placemark)
+                
+                }
+            }
+        }
+    }
+    func calculateOffSet() -> CGFloat {
+        if self.landmarks.count > 0 && !self.isTapped {
+            return UIScreen.main.bounds.size.height - UIScreen.main.bounds.size.height / 4
+        } else if self.isTapped {
+            return 100
+        } else {
+            return UIScreen.main.bounds.size.height
+        }
+    }
     
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .top) {
+        MapView(landmarks: self.landmarks)
+            TextField("Search:", text: $search) { _ in
+                self.getNearbyLandmarks()
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .offset(y: 44)
+            PlaceListView(landmarks: self.landmarks) {
+                self.isTapped.toggle()
+            }.animation(.spring())
+                .offset(y: calculateOffSet())
+        }
     }
 }
+
+struct PlaceListView: View {
+    
+    var landmarks: [Landmark]
+    var onTap: () -> ()
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                EmptyView()
+            }.frame(width: UIScreen.main.bounds.size.width, height: 60)
+                .background(Color.gray)
+                .gesture(TapGesture()
+                            .onEnded(self.onTap)
+                         )
+            List {
+                ForEach(landmarks, id:\.id) { landmark in
+                    VStack(alignment: .leading) {
+                        Text(landmark.name)
+                            .fontWeight(.bold)
+                        Text(landmark.title)
+                    }
+                }
+            }.animation(nil)
+        }.cornerRadius(10)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
